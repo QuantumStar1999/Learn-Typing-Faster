a9a1c01028ad25db6e159ec9e9c6b20f8d036d2d
Santanu <quantumstar1999@gmail.com>
2026-01-04 21:19:43 +0530

    revert

--- script.js
+++ script.js
@@ -732,7 +732,6 @@ function displayWpmHistory(currentWpm) {
     if (history.length > 0) {
         const clearButton = document.createElement('button');
         clearButton.className = 'clear-history-btn';
-        // if(!document.getElementsById("trash-button"))
         clearButton.innerHTML = '<i class="fas fa-trash-alt" id="trash-button"></i> Clear History';
         clearButton.onclick = clearWpmHistory;
         
